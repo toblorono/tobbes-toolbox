b56fb69e6f6b74448c9612ea76c7f954981aaacd
toblorono <84089176+toblorono@users.noreply.github.com>
2021-05-12 11:58:34 -0500

    Added Help Docs and Combatant Fail-Safe

--- recharge.py
+++ recharge.py
@@ -1,20 +1,38 @@
 embed
 <drac2>
-args = argparse(&ARGS&)
+args, thumb = argparse(&ARGS&), ""
 c = combat()
-recharger = c.get_combatant(args.last("t"))
-r = int(args.last("r"))
+t = args.last("t")
+r = args.last("r",6,int)
 a = args.last("a")
 chargeRoll = vroll("1d6")
 
-if chargeRoll.result.total >= r:
-    desc = a + " is recharged!" 
-    thumb = "https://i.imgur.com/IO55H1A.png"
+if not r or not a or not t or "?" in args or "help" in args:
+	title = "Need to borrow a charger?"
+	desc = """Prettify your monster recharges! 
+
+			  `!recharge -t <target> -a <ability name> -r <reroll value>`
+
+			  **__Valid Arguments__**
+			  `-t <target>` - Combatant with recharge; if no combatant is found, use raw argument.
+			  `-a <ability name>` - Recharge ability name 
+			  `-r <reroll value>` - Minimum value to recharge ability`"""
 else:
-    desc = a + " is not recharged!"
-    thumb = "https://i.imgur.com/tcQJUyM.png"
-</drac2>
--title "{{recharger.name}} attempts to recharge its {{a}}!"
+	ct = c.get_combatant(t)
+
+	if ct:
+		tName = ct.name
+	else:
+		tName = t
+	title = tName + " attempts to recharge its " + a + "!"
+	if chargeRoll.result.total >= r:
+	    desc = a + " is recharged!" 
+	    thumb = f"""-thumb "https://i.imgur.com/IO55H1A.png" -f "{a} Recharge ({r}-6)|{chargeRoll}" """
+	else:
+	    desc = a + " is not recharged!"
+	    thumb = f"""-thumb "https://i.imgur.com/tcQJUyM.png" -f "{a} Recharge ({r}-6)|{chargeRoll}" """
+	</drac2>
+-title "{{title}}"
 -desc "{{desc}}"
--f "{{a}} Recharge ({{r}}-6)|{{chargeRoll}}"
--thumb "{{thumb}}"
+{{thumb}}
+-footer "!recharge -t <target> -a <ability name> -r <reroll value>"
