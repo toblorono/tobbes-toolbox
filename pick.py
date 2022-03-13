d33a7a8795cb2a6d348d9c3ef7fa57138f397627
toblorono <84089176+toblorono@users.noreply.github.com>
2022-03-13 08:56:19 -0500

    Update pick.py
    Added option to exclude option by prefixing "$" to an option. Need to add .lower() to remove case sensitivity.

--- pick.py
+++ pick.py
@@ -1,12 +1,11 @@
 embed
 <drac2>
 c, args, f, n = combat(), &ARGS&, "", "\n"
+
 title = "PC Picker"
 desc = "This alias selects an option from previously set metadata. To set metadata, list options as arguments to this command (no flag).  Accepts multiple entries at once, separated by spaces; multiple-word entries must be wrapped in quotes. To select an option, run this alias without any arguments."
 
-# Combat 
 if c:
-    # Clear all previous options
     if "clear" in args:
         i = args.index("clear")
         args.pop(i)
@@ -14,19 +13,22 @@ if c:
         desc = f'*Previous metadata cleared*{n}{n}{desc}'
 
     # Get Mode
-    if c.get_metadata("pcPick") and not args:
+    if c.get_metadata("pcPick") and (not args or "$" in str(args)):
         cb = load_json(c.get_metadata("pcPick"))
+        if args:
+            for arg in args:
+                if "$" in arg:
+                    cb.remove(arg[1:])
+
 
         dSize = len(cb)
         dRoll = vroll("1d"+dSize)
         
         f += f'''-f "Options|{str(cb).replace("'","")[1:-1]}" '''
         f += f'-f "Roll| 1d{dSize} ({dRoll.total}) = `{cb[dRoll.total-1]}`" '
-
     # Help Mode
     elif "?" in args or "help" in args:
         title += ": Help Text"
-        
     # Set Mode
     elif args:
         cb = args
