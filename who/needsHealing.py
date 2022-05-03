94aa6331cb6190dea060facfbb6d694d11fa3b83
toblorono <84089176+toblorono@users.noreply.github.com>
2022-05-03 11:12:36 -0500

    Create needsHealing.py
    Sub-command for `!who` alias: PC Health Checker

---
+++ who/needsHealing.py
@@ -0,0 +1,25 @@
+embed <drac2>
+
+cbs, desc, n, out = combat().combatants, "", "\n", []
+
+if "&1&" != "&" + "1&":
+    thresh = float("&1&")/100
+else:
+    thresh = .333
+
+desc = f"Threshold: {round(thresh*100)}%{n}"
+
+for cb in cbs:
+    if (cb.creature_type == None and cb.hp != None and cb.hp/cb.max_hp <= thresh and not cb.get_effect("map") and not cb.get_effect("whoExclude")) or cb.get_effect("whoInclude"):
+        out.append([round(cb.hp/cb.max_hp*100,2),cb.name])
+
+out.sort()
+
+for x in out:
+    desc += f'''{n}  {x[1]}: {combat().get_combatant(x[1]).hp_str()} (**{x[0]}%**)'''
+
+if desc == f"Threshold: {round(thresh*100)}%{n}":
+    desc += f"{n}No one!"
+
+</drac2>
+-title "Who needs healing?" -desc "{{desc}}" -footer "Usage: !who needsHealing [threshold]" -thumb <image>
